e3d4637531a32410357880f52d3e743914a08fd1
bishal <bishalshrestha33@gmail.com>
2015-12-20 15:58:03 +0545

    added ticker and next prev buttons to slider

--- js/Slider.js
+++ js/Slider.js
@@ -1,31 +1,77 @@
 var strip = document.getElementsByClassName('slider-strip')[0];
 var slides = document.getElementsByClassName('slider-img');
+var btnNext = document.getElementsByClassName('btn-next')[0];
+var btnPrev = document.getElementsByClassName('btn-prev')[0];
+var bullet = document.getElementsByClassName('bullet')[0];
 var active = 0;
 var imgWidth = 1920;
 var fps = 10;
 var mlCurrent = 0;
 var mlFinal = 0;
+var intervalId;
+var slideTimeout;
+
+for(var i = 0; i<slides.length; i++){
+		var li = document.createElement('li');
+		li.id = i+'bullet';
+		li.addEventListener('click', function(){
+			active = parseInt(this.id);
+			clearTimeout(slideTimeout);
+			clearInterval(intervalId);
+			mlFinal = (imgWidth * (active) * -1);
+			bulletChanger();
+			animate(500);
+		});
+	bullet.appendChild(li);
+}
+
+var bulletChanger = function(){
+	for(var i=0 ; i<slides.length; i++){
+		document.getElementById(i+'bullet').className= '';
+	}
+	document.getElementById(active+'bullet').className = 'active';
+}
 
 var slide = function() {
 	active == slides.length-1 ? active = 0 : active++;
 	mlFinal = imgWidth * active * -1;	
+	bulletChanger();
 	animate(500);
-	setTimeout(slide, 5000);
 }
-setTimeout(slide, 3000);
+bulletChanger();
+slideTimeout = setTimeout(slide, 3000);
 
 var animate = function(duration) {
 	var counter = 0;
 	var step = (mlFinal - mlCurrent)/(duration/fps);
+	console.log('active', active);
 
-
-	var intervalId = setInterval(function(){
+	intervalId = setInterval(function(){
 		counter++;
 		mlCurrent += step;
 		strip.style.marginLeft = mlCurrent + 'px';
 		if (counter >= duration/fps) {
 			clearInterval(intervalId);
-			return;
+			slideTimeout = setTimeout(slide, 3000);
 		}
 	}, fps);
 }
+
+var onNextBtnClick = function() {
+	clearTimeout(slideTimeout);
+	clearInterval(intervalId);
+	active == slides.length-1 ? active = 0 : active++;
+	mlFinal = imgWidth * active * -1;	
+	animate(500);
+}
+
+var onPrevBtnClick = function() {
+	clearTimeout(slideTimeout);
+	clearInterval(intervalId);
+	active == 0 ? active = slides.length-1 : active--;
+	mlFinal = imgWidth * active * -1;	
+	animate(500);
+}
+
+btnNext.addEventListener('click', onNextBtnClick);
+btnPrev.addEventListener('click', onPrevBtnClick);
